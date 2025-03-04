0f6e0aa8544de418df64f602737f71115355eb54
Maciej Bartyński <maciek.bartynski@gmail.com>
2025-03-04 15:01:26 +0100

    production vite config

--- bakemania-spa/vite.config.ts
+++ bakemania-spa/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import mkcert from 'vite-plugin-mkcert'
 import os from 'os';
 
+console.log("pro", process.env.NODE_ENV)
 // https://vite.dev/config/
 export default defineConfig({
   server: {
@@ -31,9 +32,12 @@ export default defineConfig({
     }
   },
   plugins: [react(), mkcert()],
-  define: {
+  define: process.env.NODE_ENV === 'development' ? {
     "import.meta.env.VITE_API_URL": `"https://${getLocalIP()}:3000/api"`,
     "import.meta.env.VITE_WS_URL": `"https://${getLocalIP()}:3000/ws"`,
+  } : {
+    "import.meta.env.VITE_API_URL": `"https://bakemania.ovh/api"`,
+    "import.meta.env.VITE_WS_URL": `"https://bakemania.ovh/ws"`,
   },
 })
 
